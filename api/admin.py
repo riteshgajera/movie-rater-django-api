864cbc8da305b12039bf3595ed68b3dd6e970479
Ritesh Gajera <Ritesh.Gajera@ey.com>
2020-07-13 15:53:16 +0530

    Remove Preview of Image Tag

--- api/admin.py
+++ api/admin.py
@@ -3,17 +3,5 @@ from django.utils.safestring import mark_safe
 
 from .models import Movie, Rating
 
-# admin.site.register(Movie)
-@admin.register(Movie)
-class MovieAdmin(admin.ModelAdmin):
-    readonly_fields = ["movei_image"]
-
-    def movei_image(self, obj):
-        return mark_safe('<img src="{url}" width="{width}" height={height} />'.format(
-            url=obj.headshot.url,
-            width=obj.headshot.width,
-            height=obj.headshot.height,
-        ))
-
-
+admin.site.register(Movie)
 admin.site.register(Rating)
